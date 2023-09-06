87b4331ad5fc6cac09c1b14148eae812a94382dc
FogadOg <115077292+FogadOg@users.noreply.github.com>
2023-09-06 08:36:52 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,43 @@
-#hello world
+import csv
+
+class TxtToCsv():
+    def __init__(self,txtFilePath,csvSaveFilePath):
+        self.txtFilePath=txtFilePath
+        self.csvSaveFilePath=csvSaveFilePath
+        self.startTxtToCsvConvertion()
+    def startTxtToCsvConvertion(self):
+
+        txtFile = open(self.txtFilePath+".txt", 'r')
+        linesInTxtFile = txtFile.readlines()
+        arrayOfLines=self.joinLinesInToArray(linesInTxtFile)
+        self.saveArrayToCsv(arrayOfLines)
+
+    def joinLinesInToArray(self,linesInTxtFile):
+        #this method is to join the speaked line with the speaker
+        arrayOfLines=[]
+        sentance=""
+        for idx,line in enumerate(linesInTxtFile):
+            if "\n" in line and len(line)>2:
+                sentance+=line
+            else:
+                arrayOfLines.append(sentance.replace("\n",""))
+                sentance=""
+        return arrayOfLines
+
+    def saveArrayToCsv(self,linesInTxtFile):
+        with open(self.csvSaveFilePath,"w") as file:
+
+            csvFile=csv.writer(file)
+            csvFile.writerow(["input","target"])
+
+
+            previousLine=None
+            for line in linesInTxtFile:
+                if previousLine!=None:
+                    csvFile.writerow([previousLine,line])
+                    previousLine=line
+                previousLine=line
+
+            
+
+TxtToCsv("shakespeare","shakespeareTrainingData.csv")
